9751fbe5485e04b389ddee3402b07e87555cd2c2
Artem Korneenkov <artem.korneenkov@gmail.com>
2014-04-30 00:16:56 +0300

    Update pins for Ultimaker MakerPi

--- Marlin/language.h
+++ Marlin/language.h
@@ -36,6 +36,9 @@
 #elif MOTHERBOARD == 77
 	#define MACHINE_NAME "3Drag"
 	#define FIRMWARE_URL "http://3dprint.elettronicain.it/"
+#elif MOTHERBOARD == 777
+    #define MACHINE_NAME "Ultimaker"
+    #define FIRMWARE_URL "https://github.com/TheCluster/Marlin-MakerPi/"
 #else
 	#ifdef CUSTOM_MENDEL_NAME
 		#define MACHINE_NAME CUSTOM_MENDEL_NAME
